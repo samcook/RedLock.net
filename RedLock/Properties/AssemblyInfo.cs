c91aee2f57ea53fbfe0bb65210b685e9f4ba5d02
Sam Cook <github@sam.net.nz>
2015-08-09 14:21:58 +0100

    Bump version number and nuspec

--- RedLock/Properties/AssemblyInfo.cs
+++ RedLock/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RedLock.net")]
-[assembly: AssemblyCopyright("Copyright © Sam Cook 2014")]
+[assembly: AssemblyCopyright("Copyright © Sam Cook 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 
 [assembly: InternalsVisibleTo("RedLock.Tests")]
