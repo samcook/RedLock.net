eaadf01a7b266b9d4d77dde4ac03b87917cbe5c6
Sam Cook <github@sam.net.nz>
2015-09-29 11:49:27 +0100

    Remove prerelease tag from version

--- RedLock/Properties/AssemblyInfo.cs
+++ RedLock/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-test1")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
 [assembly: AssemblyFileVersion("1.5.0.0")]
 
 [assembly: InternalsVisibleTo("RedLock.Tests")]
