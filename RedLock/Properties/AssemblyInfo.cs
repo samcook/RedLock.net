dd9688790283e7c8ef542a752d10c6d55bc96530
Sam Cook <github@sam.net.nz>
2016-04-14 18:17:57 +0100

    Bump version

--- RedLock/Properties/AssemblyInfo.cs
+++ RedLock/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RedLock.net")]
-[assembly: AssemblyCopyright("Copyright © Sam Cook 2015")]
+[assembly: AssemblyCopyright("Copyright © Sam Cook 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.3")]
-[assembly: AssemblyInformationalVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3.0")]
+[assembly: AssemblyVersion("1.6.4")]
+[assembly: AssemblyInformationalVersion("1.6.4")]
+[assembly: AssemblyFileVersion("1.6.4.0")]
 
 #if !STRONG_NAME
 [assembly: InternalsVisibleTo("RedLock.Tests")]
