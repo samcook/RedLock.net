7a4d7e594c82d907ec73b5d51d7fc79b26cf836c
Sam Cook <github@sam.net.nz>
2016-05-24 10:59:23 +0100

    Bump version to 1.7.3

--- RedLock/Properties/AssemblyInfo.cs
+++ RedLock/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2")]
-[assembly: AssemblyInformationalVersion("1.7.2")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.3")]
+[assembly: AssemblyInformationalVersion("1.7.3")]
+[assembly: AssemblyFileVersion("1.7.3.0")]
 
 #if !STRONG_NAME
 [assembly: InternalsVisibleTo("RedLock.Tests")]
