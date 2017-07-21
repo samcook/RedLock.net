639fe0d20492c8d71c0b851162b9f21f8dfd0bf8
Sam Cook <github@sam.net.nz>
2017-07-21 02:33:58 +0100

    Allow configurable path to redis-server.exe for appveyor

--- RedLockNet.Tests/RedisConfig/run-server-6380.cmd
+++ RedLockNet.Tests/RedisConfig/run-server-6380.cmd
@@ -1,2 +1,13 @@
 @echo off
-start /D 6380 ..\..\packages\redis-64.3.0.500\tools\redis-server.exe redis.conf
+
+setlocal
+
+if not defined REDIS_PATH (
+	set REDIS_PATH=..\..\packages\redis-64.3.0.500\tools
+)
+
+echo %REDIS_PATH%\redis-server.exe
+
+start /D 6380 %REDIS_PATH%\redis-server.exe redis.conf
+
+endlocal
